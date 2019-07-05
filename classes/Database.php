84a8b4d79e5205096aaa3f105ad66dbf63d21fc5
Lisavanpamel <r0654553@student.thomasmore.be>
2019-07-05 22:05:42 +0200

    Database connectie
    Class Database maken
    index.php: connecteren met database

---
+++ classes/Database.php
@@ -0,0 +1,30 @@
+<?php
+class Database {
+    private static $connectie;
+
+     public static function connecteren() {
+        // SET ROOT OF THE FILES
+        if(!defined('__ROOT__')){
+            define('__ROOT__', dirname(dirname(__FILE__)));
+        }
+
+        // Instellingen.php toevoegen
+        require_once(__ROOT__.'/instellingen.php');
+
+        // Als connectie nog niet bestaat, maken we een nieuwe connectie
+        if( self::$connectie == null ){
+            self::$connectie = new PDO(
+                "mysql:host=".$instellingen['host']."; 
+                dbname=".$instellingen['databaseNaam'].";", 
+                $instellingen['gebruikersnaam'], 
+                $instellingen['paswoord']
+            );
+            echo "connectie ok"; 
+            return self::$connectie;
+        // Anders geven we huidige connectie terug
+        } else {
+            return self::$connectie;
+        }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-test 
+<?php
+// bestanden toevoegen 
+include_once("classes/Database.php"); 
+
+// connectie met db testen
+$db = new Database(); 
+$db->connecteren(); 
+?>
