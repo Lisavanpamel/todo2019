09afe7298f6606313191c451b4a2bcbcaa27f6c9
Lisavanpamel <r0654553@student.thomasmore.be>
2019-07-07 12:09:08 +0200

    file aanmaken
    footer.php
    header.php
    inloggen.php
    registreren.php
    nieuweTaak.php
    taakBewerken.php

--- index.php
+++ index.php
@@ -6,3 +6,20 @@ include_once("classes/Database.php");
 $db = new Database(); 
 $db->connecteren(); 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+    <!-- css style -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
