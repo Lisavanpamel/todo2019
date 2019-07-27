09780bf3efd13c6707ce1861764e2404abec5de9
Lisavanpamel <r0654553@student.thomasmore.be>
2019-07-27 13:37:20 +0200

    variabelen aanmak... -> klasse Lijst.php

--- classes/Lijst.php
+++ classes/Lijst.php
@@ -1,6 +1,50 @@
 <?php
-    class Lijst {
+
+// bestanden toevoegen
+include_once("classes/Database.php");
+
+class Lijst extends Database {
+        // variabelen
+        private $lijstId;
+        private $titel;
+        private $taken;
+        private $mensen;
+        private $beheerderId;
+    
+
+
+
+// lijst
+        public function getLijstId()
+        {
+                return $this->lijstId;
+        }
+
+        public function setLijstId($lijstId)
+        {
+                $this->lijstId = $lijstId;
+
+                return $this;
+        }
+
+// titel   
+        public function getTitel()
+        {
+            return $this->titel;
+        }
+
+        public function setTitel($titel)
+        {
+            $this->titel = $titel;
+
+            return $this;
+        }
+
+
+/* ////////////////// functies ////////////////// */
         
-    }
+        
+}
+
 
 ?>
--- classes/Taak.php
+++ classes/Taak.php
@@ -1,6 +1,12 @@
 <?php
-    class Taak {
+
+// bestanden toevoegen
+include_once("classes/Database.php");
+
+class Taak extends Database {
         
-    }
+    
+        
+}
 
 ?>
