8b97660c238abdb8057886e88b8150a993304a5a
Lisavanpamel <r0654553@student.thomasmore.be>
2019-07-28 21:29:13 +0200

    klasse Lijst.php
    functies toevoegen;
    - toevoegenAanDatabase
    - toevoegenAanTabelLijstGebruiker()
    - getLijstIdVanDatabase()

--- classes/Lijst.php
+++ classes/Lijst.php
@@ -54,8 +54,37 @@ class Lijst extends Database {
                 $titel = $this->getTitel();
                 $gebruikersId = $this->getGebruikersId();
 
-                        
+                        $query = $this->connecteren()->prepare("INSERT INTO lijst(titel, gebruiker)VALUES(:titel, :gebruiker)");
+                        $query->bindParam(':titel', $titel);
+                        $query->bindParam(':gebruiker', $gebruikersId);
+                        $query->execute();
         }
+
+
+        public function toevoegenAanTabelLijstGebruiker(){
+                $lijstId = $this->getLijstId();
+                $gebruikersId = $this->getGebruikersId();
+
+                // tabel lijstgebruiker (database)
+                $query = $this->connecteren()->prepare("INSERT INTO lijstgebruiker (lijstId, gebruikersId) VALUES(:lijstId, :gebruikersId)");
+                $query->bindParam(':lijstId', $lijstId);
+                $query->bindParam(':gebruikersId', $gebruikersId);
+                $query->execute();
+                // echo "OK";
+        }
+
+        public function getLijstIdVanDatabase(){
+                $titel = $this->getTitel();
+
+                        $query = $this->connecteren()->prepare("SELECT * FROM lijst WHERE titel = :titel");
+                        $query->bindParam(':titel', $titel);
+                        $query->execute();
+                        
+                        $resultaat = $query->fetch(PDO::FETCH_ASSOC);
+                        return $resultaat['id'];
+        }
+
+        
 }
 
 
