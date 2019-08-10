a17480f82592b4870f66483de5464a6fbd551fb7
Lisavanpamel <r0654553@student.thomasmore.be>
2019-08-10 11:42:40 +0200

    header.php -> Juiste naam weergeven

--- header.php
+++ header.php
@@ -4,18 +4,16 @@ include_once("classes/Database.php");
 include_once("classes/Gebruiker.php");
 $gebruiker = new Gebruiker();
 $gebruiker->setGebruikersId($_SESSION['gebruiker']);
-
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Do To</title>
-
     <!-- css style -->
     <link rel="stylesheet" href="css/style.css">
-
     <!-- lettertypes -->
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 </head>
@@ -24,7 +22,7 @@ $gebruiker->setGebruikersId($_SESSION['gebruiker']);
         <header>
             <div class="profiel">
                 <img src="images/profiel.png" alt="profielfoto" height="42" width="42">
-                <p class="naam">Lisa Van Pamel<br><a href="uitloggen.php" id="uitloggen">Uitloggen</a></p>
+                <p class="naam"><?php echo $gebruiker->zoekGebruikersNaamViaId(); ?><br><a href="uitloggen.php" id="uitloggen">Uitloggen</a></p>
             </div>
         </header>
 </body>
