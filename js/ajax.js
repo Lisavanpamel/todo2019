34357f99330b5ee4294c482b1bb6f07d8ec66bdb
Lisavanpamel <r0654553@student.thomasmore.be>
2019-08-10 14:44:00 +0200

    ajax.js vervolledigen

--- js/ajax.js
+++ js/ajax.js
@@ -31,6 +31,36 @@ $('.check').on('click', function (e) {
             .fail(function (err) {
                 console.log(err.status);
             });
+
+    } else {
+        // checkbox is niet aangevinkt
+        console.log("unchecked");
+
+        // de waarde opvragen van de checkbox
+        let taakId = $(this).attr('data-value');
+
+        // de waarde meegeven met ajax
+        $.ajax({
+            method: "POST",
+            url: "ajax/taakWegvinken.php",
+            data: { taakId: taakId }
+        })
+        // als ajax klaar is
+        .done(function (resultaat) {
+            console.log('res ' + resultaat.status);
+            if (resultaat.status == "success") {
+                // resultaat van ajax is success, dus het is gelukt
+                // verander taak van plaats
+                console.log('verander van plaats');
+
+                // zet de checkbox op checked
+                $(this).prop('checked', true);
+            }
+        })
+        // als er een fout is, toon de error
+        .fail(function (err) {
+            console.log(err.status);
+        });
     }
     
 });
