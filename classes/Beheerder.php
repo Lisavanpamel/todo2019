b43a83001837e03fbf301f75cbaedaf89bcd9050
Lisavanpamel <r0654553@student.thomasmore.be>
2019-08-16 19:00:00 +0200

    Klasse Beheerder.php

--- classes/Beheerder.php
+++ classes/Beheerder.php
@@ -1,6 +1,41 @@
 <?php
-    class Beheerder {
-        
+// bestanden toevoegen
+include_once("classes/Database.php");
+
+    class Beheerder extends Database {
+        // variabelen
+        private $gebruikersnaam;
+        private $beheerderId;
+
+
+// gebruikersnaam
+        public function getGebruikersnaam()
+        {
+                return $this->gebruikersnaam;
+        }
+
+        public function setGebruikersnaam($gebruikersnaam)
+        {
+                $this->gebruikersnaam = $gebruikersnaam;
+
+                return $this;
+        }
+
+// beheerderId
+        public function getBeheerderId()
+        {
+                return $this->beheerderId;
+        }
+
+        public function setBeheerderId($beheerderId)
+        {
+                $this->beheerderId = $beheerderId;
+
+                return $this;
+        }
+
+/* ////////////////// functies ////////////////// */
+
     }
 
 ?>
